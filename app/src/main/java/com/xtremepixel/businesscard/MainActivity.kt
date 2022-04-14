2fc44e4510f8cb6f37905dbf8e78d9cbe56ff73e
samesidem <samesidem@gmail.com>
2022-04-14 12:34:44 +0100

    Business card App completed

--- app/src/main/java/com/xtremepixel/businesscard/MainActivity.kt
+++ app/src/main/java/com/xtremepixel/businesscard/MainActivity.kt
@@ -5,17 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,6 +48,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CreateBusinessCard(){
 
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -58,15 +68,68 @@ fun CreateBusinessCard(){
                 CreateProfileImage()
                 Divider()
                 CreateProfileDetails()
-                Button(onClick = {}) {
+                Button(onClick = {
+                    buttonClickedState.value = buttonClickedState.value.not()
+                }) {
                     Text(text = "Portfolio", style = MaterialTheme.typography.button)
                     
+                }
+                
+                if (buttonClickedState.value){
+                    Content()
+                }else {
+                    Box{}
                 }
             }
 
         }
     }
 }
+
+@Composable
+fun Content(){
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Surface(modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(3.dp),
+        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+
+            Portfolio(data = listOf("Portfolio 1","Portfolio 2","Portfolio 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn{
+        items(items = data){ item ->  
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .padding(13.dp),
+            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+            elevation = 4.dp,) {
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.surface)
+                    .padding(8.dp)) {
+                    CreateProfileImage(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier.padding(7.dp).align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A developer with 10 years experience", fontWeight = FontWeight.Medium,
+                        color = Color.LightGray)
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
 private fun CreateProfileDetails() {
@@ -90,9 +153,9 @@ private fun CreateProfileDetails() {
 }
 
 @Composable
-private fun CreateProfileImage() {
+private fun CreateProfileImage(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -104,7 +167,7 @@ private fun CreateProfileImage() {
         Image(
             painter = painterResource(id = R.drawable.ic_baseline_person_pin),
             contentDescription = "Profile Image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
 
