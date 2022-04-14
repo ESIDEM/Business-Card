aab999508195c737dca186d70ed5c3753a7196de
samesidem <samesidem@gmail.com>
2022-04-14 11:09:59 +0100

    Profile information added

--- app/src/main/java/com/xtremepixel/businesscard/MainActivity.kt
+++ app/src/main/java/com/xtremepixel/businesscard/MainActivity.kt
@@ -3,18 +3,22 @@ package com.xtremepixel.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.xtremepixel.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,11 +44,70 @@ fun CreateBusinessCard(){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp)
-            .height(390.dp).padding(12.dp), elevation = 4.dp, shape = RoundedCornerShape(
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp), elevation = 4.dp, shape = RoundedCornerShape(
             corner = CornerSize(15.dp)
             )) {
+
+            Column(modifier = Modifier.height(300.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+                CreateProfileImage()
+                Divider()
+                CreateProfileDetails()
+                Button(onClick = {}) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.button)
+                    
+                }
+            }
+
         }
+    }
+}
+
+@Composable
+private fun CreateProfileDetails() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Sam Esidem",
+            style = MaterialTheme.typography.h3,
+            color = MaterialTheme.colors.primaryVariant
+        )
+        Text(
+            text = "Android Developer",
+            style = MaterialTheme.typography.h6,
+            color = Color.Gray
+        )
+        Text(
+            text = "@esidemSam",
+            style = MaterialTheme.typography.h6,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+private fun CreateProfileImage() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(width = 0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_person_pin),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+
     }
 }
 
